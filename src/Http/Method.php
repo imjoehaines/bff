abe662629ac703bae3b2c99e26c6d6e735ef2d62
Joe Haines <hello@joehaines.co.uk>
2018-11-03 10:20:32 +0000

    Add Method class

---
+++ src/Http/Method.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bff\Http;
+
+class Method
+{
+    private $value;
+
+    private function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString() : string
+    {
+        return $this->value;
+    }
+
+    public static function get() : Method
+    {
+        return new static('GET');
+    }
+
+    public static function post() : Method
+    {
+        return new static('POST');
+    }
+
+    public static function put() : Method
+    {
+        return new static('PUT');
+    }
+
+    public static function patch() : Method
+    {
+        return new static('PATCH');
+    }
+
+    public static function delete() : Method
+    {
+        return new static('DELETE');
+    }
+}
