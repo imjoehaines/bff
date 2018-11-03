e5c661485fde1c3d55529d98b7e8cfff75720180
Joe Haines <hello@joehaines.co.uk>
2018-11-03 16:39:27 +0000

    Add test for route dependencies

--- tests/Routing/RouterTest.php
+++ tests/Routing/RouterTest.php
@@ -69,6 +69,46 @@ class RouterTest extends TestCase
         $this->assertSame(400, $response->statusCode());
     }
 
+    /**
+     * @dataProvider methods
+     *
+     * @param string $method
+     * @return void
+     */
+    public function testItCanProvideDependenciesToRouteHandlers(string $method) : void
+    {
+        $path = '/';
+        $handler = function (
+            Request $request,
+            Response $response,
+            int $number,
+            string $string,
+            array $array
+        ) : Response {
+            $this->assertSame(200, $response->statusCode());
+
+            $this->assertSame(1, $number);
+            $this->assertSame('hi', $string);
+            $this->assertSame([1, 2, 3], $array);
+
+            return $response->withStatusCode(400);
+        };
+
+        $router = new Router();
+        $router->$method($path, $handler, [1, 'hi', [1, 2, 3]]);
+
+        $method = Method::$method();
+        $url = Url::from('http://example.com');
+
+        $routeHandler = $router->match($method, $url);
+
+        $response = $routeHandler->handle(
+            new Request($method, $url, new Parameters())
+        );
+
+        $this->assertSame(400, $response->statusCode());
+    }
+
     public function methods() : array
     {
         return [
